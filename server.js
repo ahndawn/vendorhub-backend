d7c1724e96e39ddb13ad5f311a673640b4d844d7
Jean Loriston <staticking29@gmail.com>
2024-02-09 14:46:14 -0500

    temp disable cors

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const dotenv = require('dotenv');
 const userRoutes = require('./src/routes/userRoutes');
 const vendorRoutes = require('./src/routes/vendorRoutes');
 const adminRoutes = require('./src/routes/adminRoutes');
-const cors = require('cors');
 
 dotenv.config();
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
@@ -19,7 +17,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use('/api/users', userRoutes);
 app.use('/api/vendors', vendorRoutes);
 app.use('/api/admin', adminRoutes);
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
